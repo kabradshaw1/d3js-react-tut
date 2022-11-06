4b2ae4668477c6905d7d445d84aca285dca7bdef
kabradshaw1 <itllbok82@gmail.com>
2022-11-05 22:27:38 -0400

    trying to figure out bundling

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello javascript')
+console.log(react)
+
+const App = () => (
+  <h1>Hello JSX</h1>
+);
+
+const rootElement = document.getElementById('root')
+ReactDom.render(<App />, rootElement);
